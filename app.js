7bfa4bab6568c701283b5da84f6722a62d26302e
Novik Yaroslav <yaroslavnowik@yandex.ru>
2023-03-08 20:46:09 +0200

    error handler added

--- app.js
+++ app.js
@@ -1,11 +1,12 @@
 const express = require('express');
 const mongoose = require('mongoose');
 const { PORT } = require('./config');
-
-const app = express();
 const { login } = require('./controllers/users');
 const { createUser } = require('./controllers/users');
 const auth = require('./middlewares/auth');
+const errorHandler = require('./middlewares/error-handler');
+
+const app = express();
 
 mongoose.set('strictQuery', false);
 
@@ -19,6 +20,7 @@ app.listen(PORT);
 app.use(express.json());
 app.post('/signin', login);
 app.post('/signup', createUser);
-
 app.use('/users', auth, require('./routers/users'));
 app.use('/cards', auth, require('./routers/cards'));
+
+app.use(errorHandler);
