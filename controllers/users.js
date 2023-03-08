9fcb1230c0ff5bb7b226115e3f5e6926ed2d1c2f
Novik Yaroslav <yaroslavnowik@yandex.ru>
2023-03-08 14:28:39 +0200

    email and password added

--- controllers/users.js
+++ controllers/users.js
@@ -1,4 +1,7 @@
-const user = require("../models/user");
+const bcrypt = require('bcrypt');
+const user = require('../models/user');
+
+const { ERROR_CODE_400, ERROR_CODE_404, ERROR_CODE_500 } = require('../utils/errors');
 
 module.exports.getUsers = (req, res) => {
   user
@@ -8,13 +11,23 @@ module.exports.getUsers = (req, res) => {
 };
 
 module.exports.createUser = (req, res) => {
-  const { name, about, avatar } = req.body;
-  user
-    .create({ name, about, avatar })
+  const {
+    name, about, avatar, email, password,
+  } = req.body;
+  bcrypt.hash(password, 10)
+    .then((hash) => user.create({
+      name, about, avatar, email, password: hash,
+    }))
     .then((newUser) => res.send({ data: newUser }))
     .catch((err) => {
-      if (err.name === "ValidationError") {
-        res.status(400).send({
+      if (!password || !email) {
+        res.status(404).send({ message: 'Заполните все обязательные поля' });
+      }
+      if (err.code === 11000) {
+        res.status(409).send({ message: 'Пользователь с такими данными уже существует' });
+      }
+      if (err.name === 'ValidationError') {
+        res.status(ERROR_CODE_400).send({
           message:
             "Переданы некорректные данные в методы создания пользователя",
         });
--- models/user.js
+++ models/user.js
@@ -1,26 +1,45 @@
-const mongoose = require("mongoose");
+const mongoose = require('mongoose');
+const validator = require('validator');
+
 const { Schema } = mongoose;
 
 const userSchema = new Schema(
   {
     name: {
       type: String,
-      required: true,
       minlength: 2,
       maxlength: 30,
+      default: 'Жак-Ив Кусто',
     },
     about: {
       type: String,
-      required: true,
       minlength: 2,
       maxlength: 30,
+      default: 'Исследователь',
     },
     avatar: {
       type: String,
+      default: 'https://pictures.s3.yandex.net/resources/jacques-cousteau_1604399756.png',
+    },
+    email: {
+      type: String,
       required: true,
+      unique: true,
+      validate: {
+        validator(isCorrect) {
+          return validator.isEmail(isCorrect);
+        },
+        message: 'Поле "email" должно быть валидным email-адресом',
+      },
+    },
+    password: {
+      type: String,
+      required: true,
+      minlength: 2,
+      maxlength: 200,
     },
   },
   { versionKey: false }
 );
 
-module.exports = mongoose.model("user", userSchema);
+module.exports = mongoose.model('user', userSchema);
