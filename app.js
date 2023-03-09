63421bd3b982b8e93d5fb2d2466012f3de9d37c3
Novik Yaroslav <yaroslavnowik@yandex.ru>
2023-03-09 16:16:30 +0200

    name and about reurement disabled

--- app.js
+++ app.js
@@ -42,8 +42,8 @@ app.post(
   celebrate({
     body: Joi.object()
       .keys({
-        name: Joi.string().required().min(2).max(30),
-        about: Joi.string().required().min(2).max(30),
+        name: Joi.string().min(2).max(30),
+        about: Joi.string().min(2).max(30),
         email: Joi.string().email().required(),
         password: Joi.string().required().min(2),
       })
